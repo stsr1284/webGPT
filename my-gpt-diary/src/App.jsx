602a43137ca558ada1fbd7136dc3c1821361693a
이동학 <donghak@idonghag-ui-MacBookPro-2.local>
2024-01-15 16:50:12 +0900

    feat: promptEngineering 적용 api의 prompt에 해당하는 messages에 promptEngineering 적용하여 system role과 user role을 보내고 CallGPT()에 prompt 파라미터를 받아서 사용자의 입력을 받을 수 있도록 함.출력문을 dummyData로 활용하여 App.jsx에 저장해둠

--- my-gpt-diary/src/App.jsx
+++ my-gpt-diary/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { CallGPT } from "./api/gpt"
 
+const dummyData = JSON.parse(
+  `{ "title": "코딩 강의 후의 고민", "summarize": "코딩 강의를 듣고 프로젝트 버그 해결", "emotional_content": "오늘 코딩 강의를 들었다. 강의 내용은 매우 유익했지만, 프로젝트에서 버그가 많이 발생했다. 스택오버플로에서 검색해보았지만 해결되지 않았다. 그런데, gpt를 활용해서 문제를 해결할 수 있었다. 하지만 이렇게 해결하는 것이 개발 실력 향상에 도움이 될까 고민이다.", "emotional_result": "이 강의를 통해 새로운 기술을 배웠고 문제를 해결할 수 있었지만, 자신의 개발 능력에 대한 의문이 들었다. 왜냐하면 버그를 해결하기 위해 다른 사람의 도움을 받았기 때문이다. 이는 내가 더 많은 경험과 지식을 쌓아야 한다는 것을 알려주는 시간이었다.", "analysis": "이러한 고민은 개발자로서 성장하고자 하는 욕구와 현재의 능력 사이에서의 갈등을 나타낸다. 개발은 끊임없이 새로운 기술과 도구가 등장하기 때문에, 다른 사람의 도움을 받는 것은 당연한 일이다. 하지만 개발자로서의 자신감과 능력을 키우기 위해서는 스스로 문제를 해결하고 발전해 나가야 한다. 이러한 경험은 나에게 중요한 성장 기회가 될 것이다.", "action_list": ["더 많은 개발 경험을 쌓는다.", "새로운 기술과 도구에 대해 학습한다.", "자주 스스로 문제를 해결해보는 시도를 한다."], "image": "coding development" }`
+)
+
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState(dummyData);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleClickAPICall = async () => {
     try {
       setIsLoading(true);
-      const message = await CallGPT();
-      setData(message);
+      const message = await CallGPT({
+        prompt: `
+      코딩 강의를 들었다. 프로젝트에 버그가 많이 나왔음, 스택오버플로에서 검색했지만 해결 안되었어.
+      역시 gpt를 통해서 해결했다. 근데 이렇게 해결하는게 개발 실력에 도움될까..?
+      `,
+      });
+      setData(JSON.parse(message));
     } catch (error) {
 
     } finally {
@@ -17,10 +26,12 @@ function App() {
     }
   };
 
+  console.log(">>data", data);
+
   return (
     <>
       <button onClick={handleClickAPICall}>GPT API CALL</button>
-      <div>data: {data}</div>
+      <div>data: {JSON.stringify(data)}</div>
       <div>isLoading: {isLoading ? "lodaing..." : "fin"}</div>
     </>
   )
--- my-gpt-diary/src/api/gpt.js
+++ my-gpt-diary/src/api/gpt.js
@@ -1,16 +1,55 @@
+/*  APIreference "makingRequests"
+curl https://api.openai.com/v1/chat/completions \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $OPENAI_API_KEY" \
+-d '{
+ "model": "gpt-3.5-turbo",
+ "messages": [{"role": "user", "content": "Say this is a test!"}],
+ "temperature": 0.7
+}'
+*/
 
-export const CallGPT = async () => {
+export const CallGPT = async ({ prompt }) => {
 	console.log("CallGPT\n");
-	/*
-	curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7
-   }'
-	*/
+	const messages = [
+		{
+			role: "system", content: `## INFO ##
+		you can add images to the reply by URL, write the image in JSON field
+		Use the Unsplash API (https://source.unsplash.com/1600x900/?). the query is just some tags that describes the image ## DO NOT RESPOND TO INFO BLOCK ##`
+		},
+		{ 
+			role: "system", content: `You are a psychological counselor who writes and analyzes emotional diaries. Proceed in the following order.`
+		},
+		{
+			role: "user",
+			content: `1. [title] : Think of the diary title after understanding the [events] separated by """ at the bottom.
+			2. [summarize] : summarize events in order with one line sentence.
+			3. [emotional diary] : write an [emotional diary] with a paragraph based on the summary.
+			4. [evaluates] : the written emotional [evaluates], using explore the unconscious based on the contents of the [emotional diary].
+			5. [Psychological analysis] : Psychological analysis is performed using professional psychological knowledge much more detailed anduse a famous quote,
+			6. [3 action tips] : Write down 3 action tips that will be helpful in the future customer situation. the three action tips must beconverted into JSON array for
+			7. [image] : Create an image by making the contents so far into one keyword.
+			
+			Translate into korean and Use the output in thw following JSON format:
+			{
+				title: here is [title],
+				summarize: here is [summarize],
+				emotional_content: here is [emotional diary],
+				emotional_result: here is [evaluates],
+				analyesis: here is [Psychological analysis],
+				action_list: here is [3 action tips],
+			}
+			
+			[events]:`,
+		},
+		{
+			role: "user",
+			content: `
+			"""
+			${prompt}
+			"""`
+		}
+	];
 	const response = await fetch("https://api.openai.com/v1/chat/completions", {
    		method: "POST",
 		headers: {
@@ -19,7 +58,7 @@ export const CallGPT = async () => {
 		},
 		body: JSON.stringify({
 			model: "gpt-3.5-turbo",
-			messages: [{ role: "user", content: "what youar name?" }],
+			messages,
 			temperature: 0.7,
 			max_tokens: 1000,
 		}),
